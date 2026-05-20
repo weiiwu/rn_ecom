e83cdc0862fc00f929c6091cea1fcfad8c4068d5
weiiwu <master_wu51@hotmail.com>
2026-05-20 15:31:54 +1000

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({message: "Success"});
+});
+
+app.listen(3000, () => console.log("Server is up and running"));
+
